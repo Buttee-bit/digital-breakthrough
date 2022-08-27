55fcee6208b93a2e585ece3555d79c9879c0a324
olegmarkelov <vvlrff@mail.ru>
2022-08-27 08:45:59 +0300

    hh

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,9 @@
+import { FileUploader } from './components/FileUploader/index'
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Reacthhhh
-        </a>
-      </header>
+      <FileUploader />
     </div>
   );
 }
